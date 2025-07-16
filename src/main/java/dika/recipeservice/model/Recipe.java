595420133d1cdc08e82360a94fb1601173ce4942
DinaraM <blueberrywitch@yandex.ru>
2025-07-16 13:21:28 +0300

    feat(RECIPE-17) add Lombok annotations for getter and setter, update column definitions, and add timestamps

--- src/main/java/dika/recipeservice/model/Recipe.java
+++ src/main/java/dika/recipeservice/model/Recipe.java
@@ -10,11 +10,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.Duration;
 import java.time.Instant;
 import java.util.UUID;
 
+
+@Getter
+@Setter
 @Entity
 @Table(name = "recipes")
 public class Recipe {
@@ -23,7 +30,7 @@ public class Recipe {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "author_external_id", nullable = false, unique = true, updatable = false, columnDefinition = "UUID")
+    @Column(name = "author_external_id", nullable = false, updatable = false, columnDefinition = "UUID")
     private UUID authorExternalId;
 
     @Column(name = "title", nullable = false, length = 100)
@@ -42,22 +49,24 @@ public class Recipe {
     @Enumerated(EnumType.STRING)
     private DifficultyLevel difficulty;
 
-    @Column(name = "prep_time", nullable = true, length = 255)
+    @Column(name = "prep_time", nullable = true)
     private Duration prepTime;
 
-    @Column(name = "cook_time", nullable = true, length = 255)
+    @Column(name = "cook_time", nullable = true)
     private Duration cookTime;
 
-    @Column(name = "servings", nullable = true, length = 255)
-    private String servings;
+    @Column(name = "servings", nullable = true)
+    private int servings;
 
     @Column(name = "status", nullable = true)
     @Enumerated(EnumType.STRING)
     private Status status;
 
+    @CreationTimestamp
     @Column(name = "created_at", nullable = true)
     private Instant createdAt;
 
+    @UpdateTimestamp
     @Column(name = "updated_at", nullable = true)
     private Instant updatedAt;
 
